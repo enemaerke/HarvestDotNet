bda69749e8a3feb539b75f3bf0f3f01309b2b0a6
Søren Enemærke <soren.enemaerke@gmail.com>
2012-11-26 08:26:05 +0100

    Adding build script

--- HarvestDotNet/Properties/AssemblyInfo.cs
+++ HarvestDotNet/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HarvestDotNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple .net library for the Harvest time tracking API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HarvestDotNet")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly:InternalsVisibleTo("HarvestDotNet.TestHarness")]
